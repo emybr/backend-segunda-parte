690e84087d4b16c0e941c1ad0998a5ae34291e11
emybr <emybr82ar@gmail.com>
2023-07-19 15:30:02 -0300

    cambio nombre

---
+++ src/dao/mongo/models1/user.models.cjs
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+
+
+const userSchema = new mongoose.Schema({
+    nombre: {
+        type: String,
+        required: true
+        
+    },
+    apellido: {
+        type: String,
+        required: true
+    },
+    edad: {
+        type: Number,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    cartId: {
+        type: [String], 
+        default: [], 
+    },
+    role: {
+        type: String,
+        enum: ['admin', 'user'],
+        default: 'user'
+    },
+    files: {
+        type: [String]
+    },
+    lastconnection: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const UserModels = mongoose.model('User', userSchema);
+
+module.exports = UserModels;
